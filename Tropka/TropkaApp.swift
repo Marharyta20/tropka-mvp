a61cfeb1b52165dea025ec10cea41c50f79c5aaf
Marharyta20 <m.stanislavava@gmail.com>
2025-05-07 10:14:34 +0200

    Initial Commit

---
+++ Tropka/TropkaApp.swift
@@ -0,0 +1,17 @@
+//
+//  TropkaApp.swift
+//  Tropka
+//
+//  Created by Marharyta Stanislavava on 07/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct TropkaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TropkaTests/TropkaTests.swift
@@ -0,0 +1,17 @@
+//
+//  TropkaTests.swift
+//  TropkaTests
+//
+//  Created by Marharyta Stanislavava on 07/05/2025.
+//
+
+import Testing
+@testable import Tropka
+
+struct TropkaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
